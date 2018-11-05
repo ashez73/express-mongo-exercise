2a7d10985591af6491d9ec72c4bdbf447ff1e753
Pawel <pawelp73@gmail.com>
2018-11-05 20:13:48 +0100

    CRUD functional

--- public/main.js
+++ public/main.js
@@ -1,18 +1,22 @@
-let update = document.getElementById('update')
-update.addEventListener('click',	event	=>	{
-	let source = document.getElementsByTagName('input')[0].value
+let method
+document.addEventListener('click',	event	=>	{
+	let name = document.getElementsByTagName('input')[0].value
 	let message = document.getElementsByTagName('input')[1].value
-	fetch('message', {
-  method: 'put',
-  headers: {'Content-Type': 'application/json'},
-  body: JSON.stringify({
-    'name': source,
-    'message': message
-  })
-}).then(res => {
-  if (res.ok) return res.json()
-})
-.then(data => {
-  console.log(data)
-})
+	if (event.target.id === 'update'||event.target.id === 'delete'){
+		method = event.target.id ==='update'? 'put' : 'delete'
+		fetch('message', {
+	  	method,
+	  	headers: {'Content-Type': 'application/json'},
+	  	body: JSON.stringify({
+	    name,
+	    message
+	  	})
+		}).then(res => {
+	  	if (res.ok) return res.json()
+		})
+		.then(data => {
+	  console.log(data)
+	  window.location.reload(true)
+		})
+	}
 })
--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const bodyParser= require('body-parser')
 const app = express()
 const hbs = require('hbs')
 const MongoClient = require('mongodb').MongoClient
+//free test db on mlab -no real data here
 const url='mongodb://Suri:Megasurion01@ds151453.mlab.com:51453/dbase'
-var db
 
+let db
 app.set('view engine', 'hbs')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended: true}))
@@ -13,23 +14,21 @@ app.use(bodyParser.json())
 
 MongoClient.connect(url,{useNewUrlParser:true}, (err, client) => {
 	if (err) return console.log(err)
-	db = client.db('dbase') // whatever your database name is
+	db = client.db('dbase') 
 	app.listen(3000, () => {
 		console.log('listening on 3000')
 	})
 })
-
 app.get('/', (req, res) => {
 	var cursor = db.collection('messages').find()
 	db.collection('messages').find().toArray(function(err, results) {
 		res.render('index', { results, base: "test" })
 	})
 })
-
 app.put('/message', (req, res) => {
 console.log(req.body)
 db.collection('messages')
-  .findOneAndUpdate({name: req.body.name,}, {
+  .findOneAndUpdate({name: req.body.name}, {
     $set: {
       name: req.body.name,
       message: req.body.message
@@ -42,15 +41,19 @@ db.collection('messages')
     res.send(result)
   })
 })
-
-
+app.delete('/message', (req, res) => {
+  db.collection('messages').findOneAndDelete({name: req.body.name, message: req.body.message}, {
+    sort: {_id: -1}
+  },
+  (err, result) => {
+    if (err) return res.send(500, err)
+    res.send({message: 'deleted!'})
+  })
+})
 app.post('/message', (req, res) => {
 	db.collection('messages').insertOne(req.body, (err, result) => {
 		if (err) return console.log(err)
-
-		console.log('saved to database')
 		res.redirect('/')
 	})
-	console.log(req.body)
 })
 
