f671842c3c713f033206742522d448d8db487667
Pawel <pawelp73@gmail.com>
2018-11-09 09:57:39 +0100

    code cleanup

--- public/main.js
+++ public/main.js
@@ -1,22 +1,22 @@
 let method
-document.addEventListener('click',	event	=>	{
+document.addEventListener('click', event => {
 	let name = document.getElementsByTagName('input')[0].value
 	let message = document.getElementsByTagName('input')[1].value
-	if (event.target.id === 'update'||event.target.id === 'delete'){
-		method = event.target.id ==='update'? 'put' : 'delete'
+	if (event.target.id === 'update' || event.target.id === 'delete') {
+		method = event.target.id === 'update' ? 'put' : 'delete'
 		fetch('message', {
-	  	method,
-	  	headers: {'Content-Type': 'application/json'},
-	  	body: JSON.stringify({
-	    name,
-	    message
-	  	})
+			method,
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({
+				name,
+				message
+			})
 		}).then(res => {
-	  	if (res.ok) return res.json()
+			if (res.ok) return res.json()
 		})
-		.then(data => {
-	  console.log(data)
-	  window.location.reload(true)
-		})
+			.then(data => {
+				console.log(data)
+				window.location.reload(true)
+			})
 	}
 })
